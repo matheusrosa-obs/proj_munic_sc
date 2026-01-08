89603a80afd666918ab6cfb798ebe5dc525016b5
matheusrwh <matheusrosa.lit@gmail.com>
2026-01-08 10:27:34 -0300

    Merge dos projetos

--- Scripts/proj_munic.py
+++ Scripts/proj_munic.py
@@ -19,13 +19,6 @@ processed_path = project_root / 'Dados/Processados'
 references_path = project_root / 'Referências'
 
 # ------------------------------------
-# CARREGAMENTO DA PROJEÇÃO DAS MESORREGIÕES ATÉ 2030
-# ------------------------------------
-df_pib_mesos_raw = pl.read_csv(interim_path / 'pib_mesos_projs.csv')
-
-df_pib_mesos_raw.head()
-
-# ------------------------------------
 # CARREGAMENTO DOS DADOS BRUTOS DOS MUNICÍPIOS E DAS MESORREGIÕES
 # ------------------------------------
 df_munic_raw = pd.read_excel(raw_path / 'pib_munic.xlsx')
@@ -43,6 +36,7 @@ df_mesos = df_mesos_raw.set_index('ano')
 df_munic.index = df_munic.index.astype(int)
 df_mesos.index = df_mesos.index.astype(int)
 
+# Cálculo dos logaritmos e diferenças
 df_munic = np.log(df_munic)
 df_munic = df_munic.diff()
 
@@ -73,21 +67,28 @@ df_munic['pib_mesoregiao'] = df_munic.apply(
     lambda row: df_mesos.loc[row['Ano'], row['Mesorregião']] if row['Mesorregião'] in df_mesos.columns else np.nan,
     axis=1)
 
-# Executa regressão para cada município e armazena os resultados em um dicionário
+# ------------------------------------
+# EXECUTA REGRESSÕES E ARMAZENA OS COEFICIENTES
+# ------------------------------------
 regression_results = {}
 
 for munic, group in df_munic.dropna(subset=['pib_munic', 'pib_mesoregiao']).groupby('Munic'):
-    model = sm.glm('pib_munic ~ pib_mesoregiao', data=group).fit(cov_type='HC0')
+    model = sm.ols('pib_munic ~ pib_mesoregiao', data=group).fit(cov_type='HC3')
     regression_results[munic] = model
 
 # Exemplo: acessar o resumo da regressão de um município específico
 print(regression_results['Florianópolis'].summary())
 
-
+# ------------------------------------
+# CARREGAMENTO DAS PROJEÇÕES DAS MESORREGIÕES
+# ------------------------------------
 proj_mesos = pd.read_excel(interim_path / 'pib_mesos_projs.xlsx')
 proj_mesos = proj_mesos.set_index('ano')
+
+# Cálculo dos logaritmos e diferenças
 proj_mesos = np.log(proj_mesos)
 proj_mesos = proj_mesos.diff()
+
 proj_mesos = proj_mesos[(proj_mesos.index >= 2003) & (proj_mesos.index <= 2030)]
 
 proj_mesos = proj_mesos.rename(columns={'pib_oeste': 'Oeste Catarinense', 'pib_norte': 'Norte Catarinense',
@@ -95,19 +96,16 @@ proj_mesos = proj_mesos.rename(columns={'pib_oeste': 'Oeste Catarinense', 'pib_n
                                     'pib_granflor': 'Grande Florianópolis', 'pib_sul': 'Sul Catarinense',
                                     'pib_sc': 'Santa Catarina'})
 
-# Prever valores de pib_munic para cada município usando os valores projetados das mesos em proj_mesos
-
 # Lista para armazenar previsões
 munic_forecasts = []
 
-# Para cada município com regressão estimada
+# ------------------------------------
+# PREDICT PARA CADA MUNICÍPIO
+# ------------------------------------
 for munic, model in regression_results.items():
     # Descobrir a mesorregião do município
     meso = df_mesos_map.loc[df_mesos_map['Munic'] == munic, 'Mesos'].values
-    if len(meso) == 0 or meso[0] not in proj_mesos.columns:
-        continue  # pula se não encontrar mesorregião ou coluna correspondente
-    meso = meso[0]
-    
+    meso = meso[0]    
     # Seleciona os anos e valores projetados da mesorregião
     mesos_proj = proj_mesos[meso]
     anos_proj = mesos_proj.index
@@ -125,9 +123,10 @@ for munic, model in regression_results.items():
 
     munic_forecasts.append(df_pred[['Ano', 'Munic', 'pib_munic_pred']])
 
-# Junta todas as previsões em um único DataFrame
+# ------------------------------------
+# EMPILHANDO AS PREVISÕES E SALVANDO EM EXCEL
+# ------------------------------------
 df_munic_forecast = pd.concat(munic_forecasts, ignore_index=True)
-
 df_munic_forecast = df_munic_forecast.pivot(index='Munic', columns='Ano', values='pib_munic_pred')
 
 df_munic_forecast.to_excel(processed_path / 'pib_munic_forecast.xlsx')
